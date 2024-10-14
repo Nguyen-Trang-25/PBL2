7c13a40c9150d8a0c5c39278568d66dd60277845
T Phuong <0511tphuong@gmail.com>
2024-10-14 16:43:48 +0700

    user.h

---
+++ include/user.h
@@ -0,0 +1,35 @@
+#ifndef USER_H
+#define USER_H
+#include <string>
+#include <ctime>
+#include "date.h"
+using namespace std;
+
+enum Sex{
+    male,
+    female,
+    unisex
+};
+
+class UserNode{
+    string idUser;
+    string name;
+    string email;
+    string phoneNum;
+    Sex sex;
+    Date birth;
+    Date createDate;
+    UserNode *next;
+};
+
+class User{
+    protected:
+        UserNode *head;
+    public:
+        void logIn();
+        void logOut();
+        void displayInfo();
+        void updateUser(); 
+        void updateStatus();
+};
+#endif
