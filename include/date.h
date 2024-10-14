6dc770970997d8cefed9e718581e512028619a1d
Nguyen-Trang-25 <nguyentrang.050925@gmail.com>
2024-10-14 16:46:23 +0700

    date.h

---
+++ include/date.h
@@ -0,0 +1,21 @@
+#ifndef DATE_H
+#define DATE_H
+#include <iostream>
+using namespace std;
+
+class Date{
+    private:
+        int day;
+        int month;
+        int year;
+        int hour;
+        int minute;
+        int second;
+    public:
+        Date(int, int, int, int = 0, int = 0, int = 0);
+        Date();
+        void setDate(int, int, int);
+        void showDate();
+};
+
+#endif
---
+++ include/user.h
@@ -0,0 +1,25 @@
+#ifndef USER_H
+#define USER_H
+#include <string>
+using namespace std;
+
+class user
+{
+private:
+    int name;
+    string id;
+public:
+    user(/* args */);
+    ~user();
+};
+
+user::user(/* args */)
+{
+}
+
+user::~user()
+{
+}
+
+
+#endif
