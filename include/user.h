9134efd6d74e329a989ce988c29d4af0bf88c8dc
Nguyen-Trang-25 <nguyentrang.050925@gmail.com>
2024-10-17 10:11:43 +0700

    Constructer user

--- include/user.h
+++ include/user.h
@@ -12,20 +12,24 @@ enum Sex{
 };
 
 class UserNode{
-    string idUser;
-    string name;
-    string email;
-    string phoneNum;
-    Sex sex;
-    Date birth;
-    Date createDate;
-    UserNode *next;
+    private:
+        string idUser;
+        string name;
+        string email;
+        string phoneNum;
+        Sex sex;
+        Date birth;
+        Date createDate;
+        UserNode *next;
+    public:
+         UserNode(string id, string n, string e, string phone, Sex s, Date b, Date cDate);
 };
 
 class User{
     protected:
         UserNode *head;
     public:
+        User();
         void logIn();
         void logOut();
         void displayInfo();
